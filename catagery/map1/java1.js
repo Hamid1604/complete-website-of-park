3dbc80f4d1df340de51ce63ea77424781e95fad2
Hamid1604 <hk5760569@gmail.com>
2024-01-08 09:28:46 +0500

    ok
    ok

---
+++ catagery/map1/java1.js
@@ -0,0 +1,30 @@
+
+
+// navbar onscroll
+
+let nav = document.querySelector(".navigation-wrap");
+window.onscroll = function() {
+    if(document.documentElement.scrollTop > 20){
+        nav.classList.add("scroll-on");
+    }
+    else{
+        nav.classList.remove("scroll-on");
+    }
+}
+
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Add an event listener to each card title to change the color to orange on click
+    const cardTitles = document.querySelectorAll('.card-title a');
+    cardTitles.forEach(title => {
+      title.addEventListener('click', function () {
+        this.style.color = 'orange';
+      });
+    });
+  });
+    
+   
---
+++ foo.js
@@ -0,0 +1,47 @@
+document.getElementById("t-footer").innerHTML +=
+`
+
+<footer >
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4 col-sm-6">
+                    <div class="single-box">
+                        <h2 class="mt-5">NATIONAL PARK</h2>
+					<div class="kk">
+						<img src="Picsart_23-11-26_15-48-28-775.png" class="img-fluid" alt="" width="150px" height="150px">
+						<p >National parks preserve and protect ecologically significant areas, fostering biodiversity and providing opportunities for outdoor recreation.</p>
+					</div>
+                        <p class="socials">
+                            <i class="fa-brands fa-facebook"></i>
+                            <i class="fa-brands fa-telegram"></i>
+                            <i class="fa-brands fa-twitter"></i>
+                            <i class="fa-brands fa-instagram"></i>
+                        </p>
+						
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-sm-6">
+                    <div class="single-box">
+                        <h2 class="mt-5 u">LINKS</h2>
+                    <ul class="paa">
+                        <li><a href="#">About Us</a></li>
+                        <li><a href="#">Book Tickets</a></li>
+                        <li><a href="#">Meet the Animals</a></li>
+                        <li><a href="#">Our Gallery</a></li>
+                    </ul>
+                    </div>                    
+                </div>
+                <div class="col-lg-4 col-sm-6">
+                    <div class="single-box">
+                        <h2 class="mt-5">CONTACT</h2>
+                    <ul class="paaa">
+                        <li><i class="fa-solid fa-phone"></i><a class="ps-1" href="#">54565878</a></li>
+                        <li><i class="fa-solid fa-envelope"></i><a class="ps-1" href="#">nationalpark@gmail.com</a></li>
+                        <li><i class="fa-solid fa-location-dot"></i><a class="ps-1" href="#">Aptech Matro Star Gate Karachi</a></li>
+                    </ul>
+                    </div>                    
+                </div>
+            </div>
+        </div>
+    </footer>`
